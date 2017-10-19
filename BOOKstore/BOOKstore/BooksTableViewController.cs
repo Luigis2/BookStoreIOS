7f461264a36955685817419a8b61d40767180157
Luigis2 <32309850+Luigis2@users.noreply.github.com>
2017-10-19 13:59:40 -0600

    FIRST W

---
+++ BOOKstore/BOOKstore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BOOKstore
+{
+    public partial class BooksTableViewController : UITableViewController
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
