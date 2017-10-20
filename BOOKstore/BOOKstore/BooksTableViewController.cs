ecd734af0dae2c0c7acc4cedeb7a08b53941155b
Luigis2 <32309850+Luigis2@users.noreply.github.com>
2017-10-20 12:43:45 -0600

    BETW@#$##

--- BOOKstore/BOOKstore/BooksTableViewController.cs
+++ BOOKstore/BOOKstore/BooksTableViewController.cs
@@ -1,13 +1,70 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace BOOKstore
 {
     public partial class BooksTableViewController : UITableViewController
     {
+        List<Book> bookList;
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
+            bookList = new List<Book>();
+
+            bookList.Add(new Book()    {
+                Author = "J.K. Rowling",
+                Name = "Harry Potter and the Sorerer's Stone",
+                Publisher = "Bloomsbury",
+                Year = 1997
+
+            });
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Chamber of Secrets",
+				Publisher = "Bloomsbury",
+				Year = 1998
+			});
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Prisoner of Azkaban",
+				Publisher = "Bloomsbury",
+				Year = 1999
+
+			});
         }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return bookList.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("Book") as BookTableViewCell;
+
+            var data = bookList[indexPath.Row];
+
+            cell.BookData = data;
+
+            return cell;
+        }
+    }
+
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public int Year;
     }
 }
